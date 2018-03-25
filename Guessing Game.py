be37a9bc4b13fc8f709339663a190cffd6036648
ashaikh23 <34725789+ashaikh23@users.noreply.github.com>
2018-03-25 18:23:58 -0400

    Add files via upload
    Cloud Backup

---
+++ Guessing Game.py
@@ -0,0 +1,24 @@
+#Copyright ® 2018 Aymaan Shaikh
+#Please Contact Before Using Code
+
+#!/usr/bin/env python3
+
+import random
+
+number = random.randint(1,20)
+
+
+
+ola = input("Hey, What's your name")
+
+print("Hello", ola + ".",)
+
+question = input("Would you like to play a game? [Y/N] ")
+
+
+
+    guess = int(input("Have a guess of which number: "))
+    if guess > number:
+        print("Guess Lower")
+        if guess < number:
+            print("Guess higher")
