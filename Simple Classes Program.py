ddec64d7e59b9d32ffdd982e33028f1a2d8f8534
ashaikh23 <34725789+ashaikh23@users.noreply.github.com>
2018-04-01 15:48:21 -0400

    Simple Classes Program

---
+++ Simple Classes Program.py
@@ -0,0 +1,16 @@
+#Copyright ® 2018 Aymaan Shaikh
+#Please Contact Before Using Code
+class One:
+    def __init__(self, name, color, weight):
+        self.name = name
+        self.color = color
+        self.weight = weight
+
+    def introduce_self(self):
+        print("My name is " + self.name)
+
+P1 = One("Tom", "red", 30)
+P2 = One("Jerry", "blue", 40)
+
+P1.introduce_self()
+P2.introduce_self()
