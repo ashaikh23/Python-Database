3d79e7448ceb32aa1475f05dede4173da71114fb
ashaikh23 <34725789+ashaikh23@users.noreply.github.com>
2018-04-16 20:22:47 -0400

    Button Test
    #Copyright ® 2018 Aymaan Shaikh
    #Please Contact Before Using Code

--- Button Test.py
+++ Button Test.py
@@ -3,15 +3,19 @@
 
 from tkinter import *
 tk = Tk()
-btn = Button(tk, text="click me")
+btn = Button(tk, text="Button1")
 btn.pack()
 import turtle
 t = turtle.Pen()
 from turtle import *
 t = Pen()
+
 def hello():
-    print("hello there")
+    print('hellow')
+
 from tkinter import *
 tk = Tk()
 btn = Button(tk, text="click me", command=hello)
 btn.pack()
+          
+             
